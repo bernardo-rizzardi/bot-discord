52137c8ddae21f870878c1307eca09c5999ae98e
Bernardo Rizzardi <rizzbarbo@gmail.com>
2026-07-15 12:37:45 -0300

    feat: bot inicial com catalogo

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Bot conectado como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '!ping':
+        await message.channel.send('pong!')
+
+client.run(TOKEN)
